830d1b8a7f45220fa2c1d3be5435983359da2d7a
Tejashree M <tejashreejuly01@gmail.com>
2026-08-04 21:34:52 +0530

    Add files via upload

---
+++ sum_list.py
@@ -0,0 +1,4 @@
+sum_list =[1,2,3,4]
+
+
+print(sum(sum_list))
---
+++ tuple_immutable.py
@@ -0,0 +1,15 @@
+"""Check that a tuple type cannot be changed in python.
+
+"""
+
+
+# t1=(1,2)
+
+
+# t1[1]= 4
+
+# print(t1)
+
+a = (34, 234, "Harry")
+
+a[2] = "Larry"
