4b9b536f7049e0a5eeb822a268586109dd6375c9
Tejashree M <tejashreejuly01@gmail.com>
2026-08-10 22:57:12 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+# i=0
+
+# for i in range(5):
+#     print(i, end =" ")
+
+# list = [1,2,3,4,5]
+
+# for i in list:
+#     print(i, end = ' ')
+
+for i in range(20,0,-1):
+    print(i, end =" ")
---
+++ func.py
@@ -0,0 +1,11 @@
+def addTwoNumbers(a,b):
+    sum = a+b
+    return sum
+
+# a=  int(input("enter the number for a : "))
+# b=  int(input("enter the number for b : "))
+
+# ans = addTwoNumbers(a,b)
+
+ans = addTwoNumbers(8476783,734678324)
+print(ans)
