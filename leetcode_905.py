303e1b800f1188e97edba87dc0bc08ac91e151d6
Tejashree M <tejashreejuly01@gmail.com>
2026-09-02 21:31:03 +0530

    Add files via upload

---
+++ leetcode_905.py
@@ -0,0 +1,27 @@
+"""905. Sort Array By Parity
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+Return any array that satisfies this condition."""
+
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+
+        n = len(nums)
+        start = 0
+
+        for i in range(n):
+
+            if nums[i] % 2 == 0:
+
+                temp = nums[i]
+                nums[i] = nums[start]
+                nums[start] = temp
+
+                start += 1
+
+        return nums
